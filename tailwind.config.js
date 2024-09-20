cb85dd8bff59f783e30e8a10190bc9fdd79f1716
Hamed Noroozi <noruzi206@gmail.com>
2024-09-20 17:28:21 +0330

    add fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-   "./index.html",
-   "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-   colors:{
-   "primary": "#FF5023",
-  },
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            "primary": "#FF5023",
+         },
+         fontFamily: {
+            'Yekan': ['YekanBakh-Regular'],
+            'Yekan-Medium': ['YekanBakh-Medium'],
+            'Yekan-Light': ['YekanBakh-Light'],
+            'Yekan-Thin': ['YekanBakh-Thin'],
+            'Yekan-Bold': ['YekanBakh-Bold'],
+            'Yekan-Heavy': ['YekanBakh-Heavy'],
+         }
+      },
+   },
+   plugins: [],
 }
 
