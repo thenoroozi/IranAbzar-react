f13807fa1e6d45472e3d2aa739eaecce8399d436
Hamed Noroozi <noruzi206@gmail.com>
2024-09-28 15:00:04 +0330

    add react query to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter } from 'react-router-dom'
-import Layout from './layouts/Layout'
 import Router from './router/Router'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import defaultOptions from "./configs/reactQuery";
+import Layout from './layouts/Layout';
 
 function App() {
 
+   const queryclient = new QueryClient({ defaultOptions })
+
    return (
-      <>
+      <QueryClientProvider client={queryclient}>
          <BrowserRouter>
             <Layout>
                <Router />
             </Layout>
          </BrowserRouter>
-      </>
+      </QueryClientProvider>
    )
 }
 
